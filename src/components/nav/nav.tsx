1dcebb260edcd80f843cd30bd1e2889ae9214147
Anubisa97 <assamsias97@gmail.com>
2024-10-20 20:06:00 +0300

    Implemented mobile burger menu

--- src/components/nav/nav.tsx
+++ src/components/nav/nav.tsx
@@ -1,9 +1,10 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
-export default function Nav({setShowSearch}) {
+export default function Nav({setShowSearch} : {setShowSearch:any}) {
 
     const [sticky, setSticky] = useState(false)
+    const [mobile, setMobile] = useState(false)
 
     const handleNavResponsiveness = () => {
         if (window.scrollY > 200) {
@@ -23,6 +24,10 @@ export default function Nav({setShowSearch}) {
     const handleShowSearch = () => {
         setShowSearch(true)
         
+    }
+
+    const toggleMobileMenu = () => {
+        setMobile(prevState => !prevState);
     }
 
     return (
@@ -64,61 +69,38 @@ export default function Nav({setShowSearch}) {
                                 <option value="tu">al</option>
                             </select>
                         </div>
-                        <Link to="#" className="btn rounded outline bg-dark medium fw-7 space signin-btn">
+                        {/* <Link to="#" className="btn rounded outline bg-dark medium fw-7 space signin-btn">
                             Sign In
-                        </Link>
+                        </Link> */}
                     </div>
-                    <div className="navbar-menu">
+                    <div className="navbar-menu" onClick={toggleMobileMenu}>
                         <i className="fa fa-bars"></i>
                     </div>
                 </div>
             </nav>
 
-            <aside id="navbar-mobile">
+            <aside id="navbar-mobile" className={mobile ? 'show' : ''}>
                 <div className="navbar-header">
                     <Link to="/" className="logo">
                         <img src="./src/assets/images/logo.png" alt="Movflx" />
                     </Link>
-                    <div className="close-btn"><i className="fa fa-times"></i></div>
+                    <div className="close-btn" onClick={toggleMobileMenu}><i className="fa fa-times"></i></div>
                 </div>
                 <ul className="navbar-body">
                     <li className="navbar-body-item parent" data-active="home">
                         <Link className="fw-5" to="/">Home</Link>
-                        <div className="dropdown-btn"><i className="fa fa-angle-down"></i></div>
-                    </li>
-                    <li className="navbar-body-item dropdown">
-                        <ul className="dropdown-box">
-                            <li className="dropdown-item mobile" data-active-child="home home-one"><Link className="fw-5" to="/">Home</Link></li>
-                        </ul>
                     </li>
                     <li className="navbar-body-item parent" data-active="movie">
                         <Link className="fw-5" to="/movies">Movies</Link>
-                        <div className="dropdown-btn"><i className="fa fa-angle-down"></i></div>
-                    </li>
-                    <li className="navbar-body-item dropdown">
-                        <ul className="dropdown-box">
-                            <li className="dropdown-item mobile" data-active-child="movie movie"><Link className="fw-5" to="/movies">Movie</Link></li>
-                            <li className="dropdown-item mobile" data-active-child="movie movie-detail"><Link className="fw-5" to="/">Movie Details</Link></li>
-                        </ul>
                     </li>
                     <li className="navbar-body-item" data-active="tv-show">
                         <Link className="fw-5" to="/shows">Tv Shows</Link>
                     </li>
                     
-                    <li className="navbar-body-item parent" data-active="blog">
-                        <Link className="fw-5" to="s/">Blog</Link>
-                        <div className="dropdown-btn"><i className="fa fa-angle-down"></i></div>
+                    <li className="navbar-body-item parent" data-active="saved">
+                        <Link className="fw-5" to="/saved">Saved</Link>
                     </li>
-                    <li className="navbar-body-item dropdown">
-                        <ul className="dropdown-box">
-                            <li className="dropdown-item mobile" data-active-child="blog blog"><Link className="fw-5" to="/">Our Blog</Link></li>
-                            <li className="dropdown-item mobile" data-active-child="blog blog-detail"><Link className="fw-5" to="/">Blog Details</Link></li>
-                        </ul>
-                    </li>
-                    <li className="navbar-body-item" data-active="contact">
-                        <Link className="fw-5" to="../contact.html">Contacts</Link>
-                    </li>
-                    <li className="navbar-body-item parent" data-active="account">
+                    {/* <li className="navbar-body-item parent" data-active="account">
                         <Link className="fw-5" to="/">Account</Link>
                         <div className="dropdown-btn"><i className="fa fa-angle-down"></i></div>
                     </li>
@@ -137,15 +119,15 @@ export default function Nav({setShowSearch}) {
                             <li className="dropdown-item"><Link className="fw-5" to="/">Search movie</Link></li>
                             <li className="dropdown-item"><Link className="fw-5" to="/">Change language</Link></li>
                         </ul>
-                    </li>
+                    </li> */}
                 </ul>
-                <ul className="navbar-footer">
+                {/* <ul className="navbar-footer">
                     <li className="navbar-footer-item"><Link to="/"><i className="fa fa-twitter"></i></Link></li>
                     <li className="navbar-footer-item"><Link to="/"><i className="fa fa-facebook"></i></Link></li>
                     <li className="navbar-footer-item"><Link to="/"><i className="fa fa-pinterest-p"></i></Link></li>
                     <li className="navbar-footer-item"><Link to="/"><i className="fa fa-instagram"></i></Link></li>
                     <li className="navbar-footer-item"><Link to="/"><i className="fa fa-youtube-play"></i></Link></li>
-                </ul>
+                </ul> */}
             </aside>
 
             <div className="overlay"></div></div>
