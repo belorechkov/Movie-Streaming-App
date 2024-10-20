f8d560283e44587e61d0c00eb3a250e948ed3e8e
Anubisa97 <assamsias97@gmail.com>
2024-10-20 20:07:26 +0300

    Working on fixing the types for the Search component

--- src/components/search/search.tsx
+++ src/components/search/search.tsx
@@ -1,17 +1,21 @@
-export default function Search({showSearch, setShowSearch}) {
+import { MouseEvent } from "react";
 
-    const hideShowSearch = () => {
-        setShowSearch(false)
+export default function Search({ showSearch, setShowSearch }: { showSearch: any, setShowSearch: any }) {
+
+    const hideShowSearch = (e: MouseEvent<HTMLDivElement, globalThis.MouseEvent>) => {
+        if (e.target.id !== 'search') {
+            setShowSearch(false)
+        }
     }
 
-    return(
-        <div>
-            <section id="search" className={showSearch ? 'show' : ''} onClick={hideShowSearch}>
-        <form action="" method="GET" className="search-box">
-            <input type="text" name="search-top" id="search-top" placeholder="Search here..."/>
-            <label htmlFor="search-top"><i className="fa fa-search"></i></label>
-        </form>
-    </section>
+    return (
+        <div onClick={e => hideShowSearch(e)}>
+            <section id="search" className={showSearch ? 'show' : ''} >
+                <form action="" method="GET" className="search-box">
+                    <input type="text" name="search-top" id="search-top" placeholder="Search here..." />
+                    <label htmlFor="search-top"><i className="fa fa-search"></i></label>
+                </form>
+            </section>
         </div>
     );
 }
