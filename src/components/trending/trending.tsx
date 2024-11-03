adbacdc43b710578e4a1ac64e750529b7f012ac6
Anubisa97 <assamsias97@gmail.com>
2024-11-03 04:10:04 +0200

    Working on implementing a standalone Carousel component

--- src/components/trending/trending.tsx
+++ src/components/trending/trending.tsx
@@ -1,84 +1,62 @@
-import { QueryClient, QueryClientProvider, useQuery } from "@tanstack/react-query"
+import { QueryClient, QueryClientProvider, useQuery } from "@tanstack/react-query";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
 
-
-import MovieCard from "../movie-card/movie-card"
+// import MovieCard from "../movie-card/movie-card"
 import CarouselCard from "../carousel-card/carousel-card";
+import Carousel from "../carousel/carousel";
 
 const settings = {
     dots: false,
-    arrows: false,
+    arrows: true,
     infinite: true,
-    speed: 500,
+    speed: 3000,
     swipeToSlide: true,
     slidesToShow: 5,
-    slidesToScroll: 1,
+    slidesToScroll: 2,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    pauseOnHover: true,
     responsive: [
         {
             breakpoint: 1600,
             settings: {
                 slidesToShow: 4,
-                // slidesToScroll: 2,
-                infinite: true,
-                dots: true,
-                arrows: false,
             },
         },
         {
             breakpoint: 1200,
             settings: {
                 slidesToShow: 3,
-                // slidesToScroll: 1,
-                infinite: true,
-                dots: false,
+                slidesToScroll: 1,
                 arrows: false,
             },
         },
         {
             breakpoint: 992,
             settings: {
-                slidesToShow: 2,
-                // slidesToScroll: 1,
-                infinite: true,
-                dots: false,
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                speed: 1500,
+                autoplaySpeed: 3000,
                 arrows: false,
             },
         },
         {
             breakpoint: 575,
             settings: {
-                slidesToShow: 1,
-                // slidesToScroll: 1,
-                infinite: true,
-                dots: false,
+                slidesToShow: 2,
+                slidesToScroll: 1,
+                speed: 1500,
+                autoplaySpeed: 3000,
                 arrows: false,
             },
         },
-        // {
-        //     breakpoint: 600,
-        //     settings: {
-        //         slidesToShow: 2,
-        //         slidesToScroll: 2,
-        //         initialSlide: 2,
-        //         arrows: false,
-        //         centerPadding: "50px", // Add this line to reduce centerPadding for smaller screens
-        //     },
-        // },
-        // {
-        //     breakpoint: 480,
-        //     settings: {
-        //         slidesToShow: 1,
-        //         slidesToScroll: 1,
-        //         arrows: false,
-        //         centerPadding: "0px", // Add this line to remove centerPadding for smallest screens
-        //     },
-        // },
     ],
 };
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
 
 // type Show = {
 //     backdrop_path: string,
@@ -94,48 +72,30 @@ export default function Trending() {
         <QueryClientProvider client={queryClient}>
             <GetTrending />
         </QueryClientProvider>
-    )
+    );
 }
 
 function GetTrending() {
     const options = {
-        method: 'GET',
+        method: "GET",
         headers: {
-            accept: 'application/json',
-            Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhNDlhM2FkYmNiYjA3YWI2Y2M4NjVhOTc1MWY3NWZhNyIsIm5iZiI6MTcyOTY5OTcxMC43NjM3MjYsInN1YiI6IjY3MGQxYTk3OWYzNTMxZTZiMjZiZTIzNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.53-1Zsz27wZ-hYeCWTXCByILOoide6JJI73JPLqZVJM'
-        }
-    };
-    const { isPending, error, data, } = useQuery({
-        queryKey: ['trending'],
-        queryFn: async () => {
-            const response = await fetch(
-                'https://api.themoviedb.org/3/trending/all/day?language=en-US', options,
-            )
-            return await response.json()
+            accept: "application/json",
+            Authorization: "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhNDlhM2FkYmNiYjA3YWI2Y2M4NjVhOTc1MWY3NWZhNyIsIm5iZiI6MTcyOTY5OTcxMC43NjM3MjYsInN1YiI6IjY3MGQxYTk3OWYzNTMxZTZiMjZiZTIzNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.53-1Zsz27wZ-hYeCWTXCByILOoide6JJI73JPLqZVJM",
         },
-    })
+    };
+    const { isPending, error, data } = useQuery({
+        queryKey: ["trending"],
+        queryFn: async () => {
+            const response = await fetch("https://api.themoviedb.org/3/trending/all/week?language=en-US", options);
+            return await response.json();
+        },
+    });
 
-    if (isPending) return 'Loading...'
+    if (isPending) return "Loading...";
 
-    if (error) return 'An error has occurred: ' + error.message
+    if (error) return "An error has occurred: " + error.message;
 
     return (
-
-        // Movies displayed with the MovieCard component
-
-        // <div className="movie-wrapper">
-        //     <ul className="movie-box grid-layout grid-card">
-        //         {data.results.map((show: {
-        //             id: number,
-        //             title: string,
-        //             overview: string
-        //         }) => (
-        //             <MovieCard name={""} first_air_date={""} poster_path={""} vote_average={0} release_date={""} key={show.id} {...show} />
-
-        //         ))}
-        //     </ul>
-        // </div>
-
         <div>
             <section id="new-release" className="suggestion">
                 <div className="suggestion-box">
@@ -143,10 +103,18 @@ function GetTrending() {
                         <p className="sub-title hightlight">Online streaming</p>
                     </div>
                     <div className="heading control">
-                        <h3 className="title center">What's Trending Today</h3>
+                        <h3 className="title center">Trending This Week</h3>
+                        <ul className="control-action">
+                            {/* <li className="action-item control">
+                                <div className="double-btn btn rounded fw-6 outline-dark small bg-dark lowercase">
+                                    <a href="#"><i className="fa fa-angle-left"></i></a>
+                                    <a href="#"><i className="fa fa-angle-right"></i></a>
+                                </div>
+                            </li> */}
+                        </ul>
                     </div>
                     <div className="movie-wrapper">
-                        <ul className="movie-box grid-layout grid-card">
+                        {/* <ul className="movie-box grid-layout grid-card">
                             {data.results.map((show: {
                                 id: number,
                                 title: string,
@@ -155,161 +123,21 @@ function GetTrending() {
                                 <MovieCard name={""} first_air_date={""} poster_path={""} vote_average={0} release_date={""} key={show.id} {...show} />
 
                             ))}
-                        </ul>
+                        </ul> */}
                     </div>
                     <div className="slick-track grid-card carousel">
-                        <Slider {...settings}>
-                            {data.results.map((show: {
-                                id: number,
-                                title: string,
-                                overview: string
-                            }) => (
+                        <Carousel data={data} />
+                        {/* <Slider {...settings}>
+                            {data.results.map((show: { id: number; title: string; overview: string }) => (
                                 <CarouselCard name={""} first_air_date={""} poster_path={""} vote_average={0} release_date={""} key={show.id} {...show} />
-
                             ))}
-                        </Slider>
+                        </Slider> */}
                     </div>
                 </div>
-
             </section>
         </div>
-    )
+    );
 }
-
-
-
-
-
-//////////////////
-
-// for page
-
-// import { QueryClient, QueryClientProvider, useQuery } from "@tanstack/react-query"
-// import MovieCard from "../movie-card/movie-card"
-
-// const queryClient = new QueryClient()
-
-// type Show = {
-//     backdrop_path: string,
-//     id: number
-//     title: string
-//     overview: string
-//     poster_path: string
-//     media_type: string
-// }
-
-// export default function Trending() {
-//     return (
-//         <QueryClientProvider client={queryClient}>
-//             <GetTrending />
-//         </QueryClientProvider>
-//     )
-// }
-
-// function GetTrending() {
-//     const options = {
-//         method: 'GET',
-//         headers: {
-//             accept: 'application/json',
-//             Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhNDlhM2FkYmNiYjA3YWI2Y2M4NjVhOTc1MWY3NWZhNyIsIm5iZiI6MTcyOTY5OTcxMC43NjM3MjYsInN1YiI6IjY3MGQxYTk3OWYzNTMxZTZiMjZiZTIzNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.53-1Zsz27wZ-hYeCWTXCByILOoide6JJI73JPLqZVJM'
-//         }
-//     };
-//     const { isPending, error, data, isFetching } = useQuery({
-//         queryKey: ['trending'],
-//         queryFn: async () => {
-//             const response = await fetch(
-//                 'https://api.themoviedb.org/3/trending/all/day?language=en-US', options,
-//             )
-//             return await response.json()
-//         },
-//     })
-
-//     if (isPending) return 'Loading...'
-
-//     if (error) return 'An error has occurred: ' + error.message
-
-//     return (
-
-//         // <div className="movie-wrapper">
-//         //     <ul className="movie-box grid-layout grid-card">
-//         //         {data.results.map((show: {
-//         //             id: number,
-//         //             title: string,
-//         //             overview: string
-//         //         }) => (
-//         //             <MovieCard name={""} first_air_date={""} poster_path={""} vote_average={0} release_date={""} key={show.id} {...show} />
-
-//         //         ))}
-//         //     </ul>
-//         // </div>
-
-//         <div>
-//             <section id="new-release" className="suggestion">
-//                 <div className="suggestion-box">
-//                     <div className="heading">
-//                         <p className="sub-title hightlight">Online streaming</p>
-//                     </div>
-//                     <div className="heading control">
-//                         <h3 className="title center">What's Trending Today</h3>
-//                         {/* <ul className="control-action">
-//                             <li className="action-item active">
-//                                 <a href="#" className="btn fw-6 rounded outline-dark small bg-gray lowercase">Animation</a>
-//                             </li>
-//                             <li className="action-item">
-//                                 <a href="#" className="btn fw-6 rounded outline-dark small bg-gray lowercase">Movies</a>
-//                             </li>
-//                             <li className="action-item">
-//                                 <a href="#" className="btn fw-6 rounded outline-dark small bg-gray lowercase">Romantic</a>
-//                             </li>
-//                             <li className="action-item">
-//                                 <select name="type-movie" title="Choose type movie"
-//                                     className="btn fw-6 dropdownable rounded outline-dark small bg-gray lowercase" defaultValue='English'>
-//                                     <option value="english">English</option>
-//                                     <option value="english">Blueray</option>
-//                                     <option value="english">4k movie</option>
-//                                     <option value="english">Hd movie</option>
-//                                 </select>
-//                             </li>
-//                         </ul> */}
-//                     </div>
-//                     <div className="movie-wrapper">
-//                         <ul className="movie-box grid-layout grid-card">
-//                             {data.results.map((show: {
-//                                 id: number,
-//                                 title: string,
-//                                 overview: string
-//                             }) => (
-//                                 <MovieCard name={""} first_air_date={""} poster_path={""} vote_average={0} release_date={""} key={show.id} {...show} />
-
-//                             ))}
-//                         </ul>
-//                     </div>
-//                     <ul className="pagination">
-//                         <li className="pagination-item active">
-//                             <a href="#" className="fw-6">1</a>
-//                         </li>
-//                         <li className="pagination-item">
-//                             <a href="#" className="fw-6">2</a>
-//                         </li>
-//                         <li className="pagination-item">
-//                             <a href="#" className="fw-6">3</a>
-//                         </li>
-//                         <li className="pagination-item">
-//                             <a href="#" className="fw-6">4</a>
-//                         </li>
-//                         <li className="pagination-item">
-//                             <a href="#" className="fw-6">Next</a>
-//                         </li>
-//                     </ul>
-//                 </div>
-//             </section>
-//         </div>
-//     )
-// }
-
-
-
-
 
 // Implement the carousel into it's own component
 // finalize the Trending component
