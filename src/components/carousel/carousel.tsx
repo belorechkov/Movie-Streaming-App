9e6a8b67caec529ed91716c22a7e528fbdc2c02e
Anubisa97 <assamsias97@gmail.com>
2024-11-03 04:22:29 +0200

    Carousel component created

--- src/components/carousel/carousel.tsx
+++ src/components/carousel/carousel.tsx
@@ -1,125 +1,65 @@
-// import React from "react";
-// import Slider from "react-slick";
-// import "slick-carousel/slick/slick.css";
-// import { Link, To } from "react-router-dom";
+// import React, { useRef } from "react";
+import Slider from "react-slick";
 
-// function SampleNextArrow(props: { className: any; style: any; onClick: any; }) {
-//     const { className, style, onClick } = props;
-//     const updatedStyle = {
-//         ...style,
-//         display: "block",
-//         background: "grey",
-//         padding: "3px",
-//         // borderRadius: "50%",
-//         width: "25px",
-//         height: "25px",
-//     };
-//     return (
-//         <div
-//             className={className}
-//             style={updatedStyle}
-//             onClick={onClick}
-//         />
-//     );
-// }
+import CarouselCard from "../carousel-card/carousel-card";
 
-// function SamplePrevArrow(props: { className: any; style: any; onClick: any; }) {
-//     const { className, style, onClick } = props;
-//     const updatedStyle = {
-//         ...style,
-//         display: "block",
-//         background: "grey",
-//         padding: "3px",
-//         //   borderRadius: "50%",
-//         width: "25px",
-//         height: "25px",
-//     };
-
-//     return (
-//         <div
-//             className={className}
-//             style={updatedStyle}
-//             onClick={onClick}
-//         />
-//     );
-// }
-
-// export default function SimpleSlider({ slides, slidesToShow, centerPadding }) {
-
-//     var settings = {
-//         // className: "center",
-//         dots: true,
-//         infinite: true,
-//         speed: 2000,
-//         arrows: true,
-//         autoplay: true,
-//         autoplaySpeed: 3000,
-//         slidesToShow: slidesToShow,
-//         pauseOnHover: true,
-//         centerMode: true,
-//         centerPadding: centerPadding,
-//         nextArrow: <SampleNextArrow className={undefined} style={undefined} onClick={undefined} />,
-//         prevArrow: <SamplePrevArrow className={undefined} style={undefined} onClick={undefined} />,
-//         responsive: [
-//             {
-//                 breakpoint: 1024,
-//                 settings: {
-//                     slidesToShow: 3,
-//                     slidesToScroll: 3,
-//                     infinite: true,
-//                     dots: true,
-//                     arrows: false,
-//                 }
-//             },
-//             {
-//                 breakpoint: 600,
-//                 settings: {
-//                     slidesToShow: 2,
-//                     slidesToScroll: 2,
-//                     initialSlide: 2,
-//                     arrows: false,
-//                 }
-//             },
-//             {
-//                 breakpoint: 480,
-//                 settings: {
-//                     slidesToShow: 1,
-//                     slidesToScroll: 1,
-//                     arrows: false,
-//                 }
-//             }
-//         ],
-//         appendDots: (dots: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | React.ReactPortal | null | undefined) => (
-//             <ul
-//                 style={{
-//                     display: "inline-block",
-//                     margin: "0 5px",
-//                     padding: "5px",
-//                     borderRadius: "25%",
-//                     cursor: "pointer",
-//                 }}
-//             >
-//                 {dots}
-//             </ul>
-//         ),
-//     };
+export default function Carousel({ data }) {
 
 
-//     return (
-//         <>
-//             <Slider  {...settings} className="mb-10 rounded-xl relative">
-//                 {slides.map((slide: { id: React.Key | null | undefined; link: To; backdrop_path: string | undefined; }) => (
-//                     <div className="h-[16rem] py-[1rem] px-[0.25rem]" key={slide.id}>
-//                         <Link to={slide.link}>
-//                             <img
-//                                 className="w-full rounded-xl hover:scale-110"
-//                                 src={slide.backdrop_path}
-//                                 alt={`slide ${slide.id}`}
-//                             />
-//                         </Link>
-//                     </div>
-//                 ))}
-//             </Slider>
-//         </>
-//     );
-// }
+    const settings = {
+        dots: false,
+        arrows: true,
+        infinite: true,
+        speed: 3000,
+        swipeToSlide: true,
+        slidesToShow: 5,
+        slidesToScroll: 2,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        pauseOnHover: true,
+        responsive: [
+            {
+                breakpoint: 1600,
+                settings: {
+                    slidesToShow: 4,
+                },
+            },
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    arrows: false,
+                },
+            },
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    speed: 1500,
+                    autoplaySpeed: 3000,
+                    arrows: false,
+                },
+            },
+            {
+                breakpoint: 575,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    speed: 1500,
+                    autoplaySpeed: 3000,
+                    arrows: false,
+                },
+            },
+        ],
+    };
+
+    return (
+        <Slider {...settings}>
+            {data.results.map((show: { id: number; title: string; overview: string }) => (
+                <CarouselCard name={""} first_air_date={""} poster_path={""} vote_average={0} release_date={""} key={show.id} {...show} />
+            ))}
+        </Slider>
+    )
+}
--- src/components/trending/trending.tsx
+++ src/components/trending/trending.tsx
@@ -1,60 +1,60 @@
 import { QueryClient, QueryClientProvider, useQuery } from "@tanstack/react-query";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import Slider from "react-slick";
+// import Slider from "react-slick";
 
 // import MovieCard from "../movie-card/movie-card"
-import CarouselCard from "../carousel-card/carousel-card";
+// import CarouselCard from "../carousel-card/carousel-card";
 import Carousel from "../carousel/carousel";
 
-const settings = {
-    dots: false,
-    arrows: true,
-    infinite: true,
-    speed: 3000,
-    swipeToSlide: true,
-    slidesToShow: 5,
-    slidesToScroll: 2,
-    autoplay: true,
-    autoplaySpeed: 5000,
-    pauseOnHover: true,
-    responsive: [
-        {
-            breakpoint: 1600,
-            settings: {
-                slidesToShow: 4,
-            },
-        },
-        {
-            breakpoint: 1200,
-            settings: {
-                slidesToShow: 3,
-                slidesToScroll: 1,
-                arrows: false,
-            },
-        },
-        {
-            breakpoint: 992,
-            settings: {
-                slidesToShow: 3,
-                slidesToScroll: 1,
-                speed: 1500,
-                autoplaySpeed: 3000,
-                arrows: false,
-            },
-        },
-        {
-            breakpoint: 575,
-            settings: {
-                slidesToShow: 2,
-                slidesToScroll: 1,
-                speed: 1500,
-                autoplaySpeed: 3000,
-                arrows: false,
-            },
-        },
-    ],
-};
+// const settings = {
+//     dots: false,
+//     arrows: true,
+//     infinite: true,
+//     speed: 3000,
+//     swipeToSlide: true,
+//     slidesToShow: 5,
+//     slidesToScroll: 2,
+//     autoplay: true,
+//     autoplaySpeed: 5000,
+//     pauseOnHover: true,
+//     responsive: [
+//         {
+//             breakpoint: 1600,
+//             settings: {
+//                 slidesToShow: 4,
+//             },
+//         },
+//         {
+//             breakpoint: 1200,
+//             settings: {
+//                 slidesToShow: 3,
+//                 slidesToScroll: 1,
+//                 arrows: false,
+//             },
+//         },
+//         {
+//             breakpoint: 992,
+//             settings: {
+//                 slidesToShow: 3,
+//                 slidesToScroll: 1,
+//                 speed: 1500,
+//                 autoplaySpeed: 3000,
+//                 arrows: false,
+//             },
+//         },
+//         {
+//             breakpoint: 575,
+//             settings: {
+//                 slidesToShow: 2,
+//                 slidesToScroll: 1,
+//                 speed: 1500,
+//                 autoplaySpeed: 3000,
+//                 arrows: false,
+//             },
+//         },
+//     ],
+// };
 
 const queryClient = new QueryClient();
 
@@ -127,11 +127,6 @@ function GetTrending() {
                     </div>
                     <div className="slick-track grid-card carousel">
                         <Carousel data={data} />
-                        {/* <Slider {...settings}>
-                            {data.results.map((show: { id: number; title: string; overview: string }) => (
-                                <CarouselCard name={""} first_air_date={""} poster_path={""} vote_average={0} release_date={""} key={show.id} {...show} />
-                            ))}
-                        </Slider> */}
                     </div>
                 </div>
             </section>
@@ -139,6 +134,3 @@ function GetTrending() {
     );
 }
 
-// Implement the carousel into it's own component
-// finalize the Trending component
-// Crete Popular Movies and Popular TV Shows Components
