dca00a910d6b876a408c5c6a3c457c8811230442
Anubisa97 <assamsias97@gmail.com>
2024-11-22 07:47:41 +0200

    implementing a close function for the wrapper

--- src/components/details/details.tsx
+++ src/components/details/details.tsx
@@ -28,7 +28,7 @@ function GetMovieDetails() {
     const [ShowMedia, setShowMedia] = useState(false);
 
     const handleWatchMedia = () => {
-        setShowMedia(prevState => !prevState);
+        setShowMedia(prevState => !prevState); 
     };
 
     const { showType, id } = useParams();
@@ -71,12 +71,12 @@ function GetMovieDetails() {
 
 
     return (
-        <div >
-            {/* <div onClick={ShowMedia && handleWatchMedia} id="close-media-wrapper"> */}
-            {/* {ShowMedia && <MediaWrapper MediaURL={"https://vidsrc.xyz/embed/" + showType + "/" + id} onClose={() => handleWatchMedia}/>} */}
+        <div onClick={ShowMedia && handleWatchMedia}>
+            {/* {ShowMedia && <button className="close-wrapper-button"></button>} */}
+             {ShowMedia && <MediaWrapper MediaURL={"https://vidsrc.xyz/embed/" + showType + "/" + id}/>}
+             {/* {ShowMedia && <MediaWrapper MediaURL={"https://vidsrc.xyz/embed/" + showType + "/" + id} onClose={() => handleWatchMedia}/>} */}
 
-            <section id="movie-banner" onClick={ShowMedia && handleWatchMedia}>
-                {ShowMedia && <MediaWrapper MediaURL={"http://localhost:5173/details/tv/34634"} />}
+            <section id="movie-banner">
                 <div className="movie-banner-box">
                     <div className="image thumb-image">
                         <img className="" src={"https://image.tmdb.org/t/p/w500//" + data.poster_path} alt="movie" />
--- src/components/media-wrapper/media-wrapper.tsx
+++ src/components/media-wrapper/media-wrapper.tsx
@@ -1,9 +1,9 @@
 
 export default function MediaWrapper({ MediaURL}: { MediaURL: string; }) {
 
-
     return (
         <section id="wrapper" className='trailer-wrapper show'>
+            <button className="close-wrapper-button"></button>
             <div className="video-box">
                 <iframe className="video" src={MediaURL} title="" frameBorder="0"
                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
